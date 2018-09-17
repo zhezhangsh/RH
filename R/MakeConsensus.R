0c785b0f9e9ff2682c12db5f377fa68aee5975be
Zhang <zhangz@email.chop.edu>
2018-09-17 11:38:37 -0400

    consensus

--- R/MakeConsensus.R
+++ R/MakeConsensus.R
@@ -12,10 +12,10 @@ WeightConsensus <- function(mtrx, method=c('rank', 'r', 'count')) {
     n - 1;
   });
   if (tolower(method)[1] == 'rank') weight <- rank(-colMeans(cnt)) else
-    if (tolower(method)[1] == 'r') weight <- colMeans(cnt)/(ncol(cnt)-1) else 
+    if (tolower(method)[1] == 'r') weight <- colMeans(cnt)/(ncol(cnt)-1) else
       if (tolower(method)[1] == 'count') weight <- colMeans(cnt) else
         weight <- rep(1, ncol(cnt));
-      
+
   for (i in 1:ncol(mtrx)) {
     x <- mtrx[, i];
     rng <- range(which(x!='-'));
@@ -30,7 +30,7 @@ WeightConsensus <- function(mtrx, method=c('rank', 'r', 'count')) {
 
   out <- cbind(tbl, Count=ttl, Gap=apply(mtrx, 1, function(x) length(x[x=='-'])));
 
-  list(weighted=out, weight=weight);
+  list(weighted=out, weight=weight, method=method);
 };
 
 CallConsensus <- function(weighted) {
@@ -47,9 +47,9 @@ CallConsensus <- function(weighted) {
 AlignConsensus <- function(con, ref) {
   require(S4Vectors);
   require(Biostrings);
-  
-  pw <- pairwiseAlignment(con, ref);
-  
+
+  pw <- pairwiseAlignment(con, ref, type='global-local');
+
   ptn <- as.character(pattern(pw));
   sbj <- as.character(subject(pw));
   prs <- data.frame(P=strsplit(ptn, '')[[1]], S=strsplit(sbj, '')[[1]], stringsAsFactors = FALSE);
@@ -57,7 +57,7 @@ AlignConsensus <- function(con, ref) {
   ind[prs[,1]!='-'] <- 1:length(prs[prs[,1]!='-', 1]);
   rownames(prs) <- 1:nrow(prs);
   prs$index <- ind;
-  
+
   ind0 <- prs[prs[,1]==prs[,2] & prs[,1]!='-', 3];
   ind1 <- prs[prs[,1]!='-' & prs[,2]!='-' & prs[,1]!=prs[,2], 3];
   ind2 <- prs[prs[,2]=='-', 3];
@@ -65,13 +65,13 @@ AlignConsensus <- function(con, ref) {
   ind3 <- ind3[!is.na(ind3)];
   ind4 <- Rle(as.integer(prs[,1]=='-'));
   ind4 <- runLength(ind4)[runValue(ind4)==1];
-  
-  cnt <- c(match=nmatch(pw), mismatch=nmismatch(pw), insertion=nindel(pw)@insertion[1,], 
+
+  cnt <- c(match=nmatch(pw), mismatch=nmismatch(pw), insertion=nindel(pw)@insertion[1,],
            deletion=nindel(pw)@deletion[1,]);
   idl <- nindel(pw);
-  
-  list(alignment=pw, 
-       index=list(match=ind0, mismatch=ind1, insertion=ind2, deletion=ind3, deletion.width=ind4), 
+
+  list(alignment=pw,
+       index=list(match=ind0, mismatch=ind1, insertion=ind2, deletion=ind3, deletion.width=ind4),
        count=c(match=nmatch(pw), mismatch=nmismatch(pw), insertion=idl@insertion[1,], deletion=idl@deletion[1,]));
 }
 
@@ -82,7 +82,7 @@ OptimizeConsensus <- function(freq, count, gap, ref) {
     mx <- which(x==max(x));
     if (length(mx)>1) sample(mx, 1) else mx;
   };
-  
+
   lvl1 <- unique(count);
 }
 
